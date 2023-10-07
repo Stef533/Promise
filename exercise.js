bb26b7a3cedd877423cf14d280dd5c49cb50f71d
Stef533 <137123241+Stef533@users.noreply.github.com>
2023-10-07 16:28:48 +0200

    Add files via upload

---
+++ exercise.js
@@ -0,0 +1,20 @@
+const number = 10; 
+
+const checkNumber = (number) => {
+    return new Promise((resolve, reject) => {
+      if (number > 10) {
+        resolve(`${number} è maggiori di 10.`);
+      } else {
+        reject(`${number} è minore di 10.`);
+      }
+    });
+  };
+    
+  checkNumber(number)
+    .then((message) => {
+      console.log(message);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+  
